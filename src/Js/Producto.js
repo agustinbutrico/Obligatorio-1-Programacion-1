b944c04ab6ec781a40f91bb7b4357210a4c71a7d
Agus_But <agustinbutrico@gmail.com>
2024-06-04 23:39:04 -0300

    Correcciones
    Se eliminó una carpeta innecesaria.

--- src/Js/Producto.js
+++ src/Js/Producto.js
@@ -1,12 +1,12 @@
 class Producto {
-    constructor(_nombre, _precio, _descripcion, _imagen, _stock, _estado, _oferta, _id,) {
-        this.nombre = _nombre;
-        this.precio = _precio;
-        this.descripcion = _descripcion;
-        this.imagen = _imagen;
-        this.stock = _stock;
-        this.estado = _estado;
-        this.oferta = _oferta;
-        this.id = _id;
-    }
+  constructor(_nombre, _precio, _descripcion, _imagen, _stock, _estado, _oferta, _id) {
+    this.nombre = _nombre;
+    this.precio = _precio;
+    this.descripcion = _descripcion;
+    this.imagen = _imagen;
+    this.stock = _stock;
+    this.estado = _estado;
+    this.oferta = _oferta;
+    this.id = _id;
+  }
 }
