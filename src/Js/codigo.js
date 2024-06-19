43d28cee8f443373b2f5381ddc4f85fe6595c400
Agus_But <agustinbutrico@gmail.com>
2024-06-18 21:15:22 -0300

    Compra de productos
    En listarProductos() se creó la validación de stock mayor a 0 y estado activo.

--- src/Js/codigo.js
+++ src/Js/codigo.js
@@ -139,26 +139,29 @@ function listarProductos() {
   let cuerpoTablaOferta = "";
   for (i = 0; i < sis.Productos.length; i++) {
     let prod = sis.Productos[i];
-    cuerpoTabla += `<tr>
-        <td><img src="${prod.imagen}"></td>
-        <td>${prod.nombre}</td>
-        <td>${prod.descripcion}</td>
-        <td><label for="numCantUnidades${prod.id}"><input type="number" id="numCantUnidades${prod.id}" min=1 value=1></td>
-        <td>${prod.precio}</td>
-        <td></td>
-        <td><input type="button" value="Comprar" class="btnAgregarCompra" data-id-producto="${prod.id}"></td>
-      </tr>`;
-    if (prod.oferta === 1) {
-      let precioConDescuento = descuentoFijo(prod.precio, 20);
-      cuerpoTablaOferta += `<tr>
-        <td><img src="${prod.imagen}"></td>
-        <td>${prod.nombre}</td>
-        <td>${prod.descripcion}</td>
-        <td><label for="numCantUnidadesOferta${prod.id}"><input type="number" id="numCantUnidadesOferta${prod.id}" min=1 value=1></td>
-        <td>${prod.precio}</td>
-        <td>20% OFF ${precioConDescuento.toFixed(0)}</td>
-        <td><input type="button" value="Comprar" class="btnAgregarCompraOferta" data-id-producto="${prod.id}"></td>
-      </tr>`;
+    if (prod.stock <= 0 || prod.estado === 0) {
+    } else {
+      cuerpoTabla += `<tr>
+      <td><img src="${prod.imagen}"></td>
+      <td>${prod.nombre}</td>
+      <td>${prod.descripcion}</td>
+      <td><label for="numCantUnidades${prod.id}"><input type="number" id="numCantUnidades${prod.id}" min=1 value=1></td>
+      <td>${prod.precio}</td>
+      <td></td>
+      <td><input type="button" value="Comprar" class="btnAgregarCompra" data-id-producto="${prod.id}"></td>
+    </tr>`;
+      if (prod.oferta === 1) {
+        let precioConDescuento = descuentoFijo(prod.precio, 20);
+        cuerpoTablaOferta += `<tr>
+      <td><img src="${prod.imagen}"></td>
+      <td>${prod.nombre}</td>
+      <td>${prod.descripcion}</td>
+      <td><label for="numCantUnidadesOferta${prod.id}"><input type="number" id="numCantUnidadesOferta${prod.id}" min=1 value=1></td>
+      <td>${prod.precio}</td>
+      <td>20% OFF ${precioConDescuento.toFixed(0)}</td>
+      <td><input type="button" value="Comprar" class="btnAgregarCompraOferta" data-id-producto="${prod.id}"></td>
+    </tr>`;
+      }
     }
   }
   document.querySelector("#curpoProductos").innerHTML = cuerpoTabla;
